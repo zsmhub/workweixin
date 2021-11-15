7130f51907c5efdf7cef26175767a82a5b5c11e1
zsm <zhangshimiande@gmail.com>
2021-11-15 16:16:56 +0800

    add callback

---
+++ callbacks/event修改设置工作台自定义开关事件推送.go
@@ -0,0 +1,59 @@
+package callbacks
+
+import "encoding/xml"
+
+// 自动生成的回调结构，按需修改, 生成方式: make callback doc=微信文档地址url
+// 文档: https://work.weixin.qq.com/api/doc/90001/90143/94620#修改设置工作台自定义开关事件推送
+
+func init() {
+	// 添加可解析的回调事件
+	supportCallback(EventSwitchWorkbenchMode{})
+}
+
+type EventSwitchWorkbenchMode struct {
+	XMLName    xml.Name `xml:"xml"`
+	Text       string   `xml:",chardata"`
+	ToUserName struct {
+		Text string `xml:",chardata"`
+	} `xml:"ToUserName"`
+	FromUserName struct {
+		Text string `xml:",chardata"`
+	} `xml:"FromUserName"`
+	CreateTime struct {
+		Text string `xml:",chardata"`
+	} `xml:"CreateTime"`
+	MsgType struct {
+		Text string `xml:",chardata"`
+	} `xml:"MsgType"`
+	Event struct {
+		Text string `xml:",chardata"`
+	} `xml:"Event"`
+	Mode struct {
+		Text string `xml:",chardata"`
+	} `xml:"Mode"`
+	AgentID struct {
+		Text string `xml:",chardata"`
+	} `xml:"AgentID"`
+}
+
+func (EventSwitchWorkbenchMode) GetMessageType() string {
+	return "event"
+}
+
+func (EventSwitchWorkbenchMode) GetEventType() string {
+	return "switch_workbench_mode"
+}
+
+func (EventSwitchWorkbenchMode) GetChangeType() string {
+	return ""
+}
+
+func (m EventSwitchWorkbenchMode) GetTypeKey() string {
+	return m.GetMessageType() + ":" + m.GetEventType() + ":" + m.GetChangeType()
+}
+
+func (EventSwitchWorkbenchMode) ParseFromXml(data []byte) (CallBackExtraInfoInterface, error) {
+	var temp EventSwitchWorkbenchMode
+	err := xml.Unmarshal(data, &temp)
+	return temp, err
+}
