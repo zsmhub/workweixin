4fc1ca42f57cad2fc17a1ed44b4e873dc8f1a8d6
zsm <zhangshimiande@gmail.com>
2022-08-09 10:08:50 +0800

    更新接口调用许可的API

--- apis/接口调用许可-获取激活码详情.go
+++ apis/接口调用许可-获取激活码详情.go
@@ -35,12 +35,19 @@ type RespGetActiveInfoByCodeLicense struct {
 	ActiveInfo struct {
 		// ActiveCode 帐号激活码
 		ActiveCode string `json:"active_code"`
-		ActiveTime int    `json:"active_time"`
+		// ActiveTime 首次激活绑定用户的时间，未激活则不返回该字段
+		ActiveTime int `json:"active_time"`
 		// CreateTime 创建时间，订单支付成功后立即创建。激活码必须在创建时间后的365天内激活。
 		CreateTime int `json:"create_time"`
 		// ExpireTime 过期时间。为首次激活绑定的时间加上购买时长。未激活则不返回该字段
 		ExpireTime int `json:"expire_time"`
-		// Status 帐号状态:1:未绑定，2:已绑定且有效，3:已过期
+		MergeInfo  struct {
+			// FromActiveCode 激活码激活userid时，若userid原来已经绑定了一个激活码，则会返回该字段
+			FromActiveCode string `json:"from_active_code"`
+			// ToActiveCode 该激活码合并到的新激活码信息
+			ToActiveCode string `json:"to_active_code"`
+		} `json:"merge_info"` // 合并信息，合并的激活码或者被合并的激活码才返回该字段。功能灰度中，目前仅通过api新续期过的才一定返回
+		// Status 帐号状态:1:未绑定，2:已绑定且有效，3:已过期，4:待转移，5:已合并（激活码本身激活了userid，后续使用新的激活码重新激活了该userid，则该码变为已合并状态。若被合并时，该激活码未过期则合并后会重置expire_time为合并时间。若被合并时，激活码已过期则不重置expire_time）
 		Status int `json:"status"`
 		// Type 帐号类型:1:基础帐号，2:互通帐号
 		Type int `json:"type"`
@@ -103,12 +110,19 @@ type RespBatchGetActiveInfoByCodeLicense struct {
 	ActiveInfoList []struct {
 		// ActiveCode 帐号激活码
 		ActiveCode string `json:"active_code"`
-		ActiveTime int    `json:"active_time"`
+		// ActiveTime 首次激活绑定用户的时间，未激活则不返回该字段
+		ActiveTime int `json:"active_time"`
 		// CreateTime 创建时间，订单支付成功后立即创建。激活码必须在创建时间后的365天内激活
 		CreateTime int `json:"create_time"`
 		// ExpireTime 过期时间。为首次激活绑定的时间加上购买时长。未激活则不返回该字段
 		ExpireTime int `json:"expire_time"`
-		// Status 帐号状态:1:未绑定，2:已绑定且有效，3:已过期
+		MergeInfo  struct {
+			// FromActiveCode 合并信息，合并的激活码或者被合并的激活码才返回该字段。功能灰度中，目前仅通过api新续期过的才一定返回
+			FromActiveCode string `json:"from_active_code"`
+			// ToActiveCode 该激活码合并到的新激活码信息
+			ToActiveCode string `json:"to_active_code"`
+		} `json:"merge_info"` // 合并信息。功能灰度中，目前仅通过api新续期过的才一定返回
+		// Status 帐号状态:1:未绑定，2:已绑定且有效，3:已过期，4:待转移，5:已合并（激活码本身激活了userid，后续使用新的激活码重新激活了该userid，则该码变为已合并状态。若被合并时，该激活码未过期则合并后会重置expire_time为合并时间。若被合并时，激活码已过期则不重置expire_time）
 		Status int `json:"status"`
 		// Type 帐号类型:1:基础帐号，2:互通帐号
 		Type int `json:"type"`
